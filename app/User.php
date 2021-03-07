89fa681f8f4e37a896263150f53daeda0f6c2a22
EC2 Default User <ec2-user@ip-172-31-20-9.us-east-2.compute.internal>
2021-03-07 05:08:14 +0000

    migration実行

--- app/User.php
+++ app/User.php
@@ -9,12 +9,14 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
+
+    protected $table = 'users';
+
     protected $fillable = [
         'name', 'email', 'password',
     ];
